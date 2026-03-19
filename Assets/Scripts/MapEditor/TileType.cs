2d97fc782c93ea2e0a32126954e88ab4b6960b2a
MozzarellaKouki <koukix12345@icloud.com>
2026-03-19 20:18:23 +0900

    プレビュー追加と安定性向上

--- Assets/Scripts/MapEditor/TileType.cs
+++ Assets/Scripts/MapEditor/TileType.cs
@@ -7,15 +7,5 @@ public class TileType : MonoBehaviour
 
 public enum TileTypeEnum
 {
-    Block_01,
-    Block_02,
-    Block_03,
-    Block_04,
-    Block_05,
-    Block_06,
-    Block_07,
-    Block_08,
-    Block_09,
-
     MAX
 }
