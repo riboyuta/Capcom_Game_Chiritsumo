03b5b88d6cb2324d9bf96e9fe1360a09866c7128
riboyuta <riyuta0408@gmail.com>
2026-03-27 10:54:34 +0900

    タイトル　キーボードとコントローラー対応

--- Assets/Scripts/Core/Boot/BootSceneController.cs
+++ Assets/Scripts/Core/Boot/BootSceneController.cs
@@ -5,22 +5,6 @@ public sealed class BootSceneController : MonoBehaviour
 {
     public static BootSceneController Instance { get; private set; }
 
-    [Header("Input")]
-    [SerializeField]
-    private RawInputSource _rawInputSource;
-
-    [SerializeField]
-    private SystemInputBindings _systemInputBindings = new SystemInputBindings();
-
-    [SerializeField]
-    private DebugInputBindings _debugInputBindings = new DebugInputBindings();
-
-    private SystemInputReader _systemInputReader;
-    private DebugInputReader _debugInputReader;
-
-    public SystemInputReader SystemInput => _systemInputReader;
-    public DebugInputReader DebugInput => _debugInputReader;
-
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -31,31 +15,11 @@ public sealed class BootSceneController : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
-
-        if (_rawInputSource == null)
-        {
-            _rawInputSource = GetComponent<RawInputSource>();
-        }
-
-        if (_rawInputSource == null)
-        {
-            Debug.LogError("[BootSceneController] RawInputSource is required.", this);
-            enabled = false;
-            return;
-        }
-
-        _systemInputReader = new SystemInputReader(_rawInputSource, _systemInputBindings);
-        _debugInputReader = new DebugInputReader(_rawInputSource, _debugInputBindings);
     }
 
     private void Update()
     {
-        if (_systemInputReader == null)
-        {
-            return;
-        }
 
-        _systemInputReader.Update();
     }
 
     private void Start()
